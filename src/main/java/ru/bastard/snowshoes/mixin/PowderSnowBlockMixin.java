ced2617b11067b2c67831d05f20233d3440996ff
BastardG <bamboo03-nv@mail.ru>
2023-10-26 07:35:50 +0500

    Updated texture, fixed colliding with Powdered Snow from sides

--- src/main/java/ru/bastard/snowshoes/mixin/PowderSnowBlockMixin.java
+++ src/main/java/ru/bastard/snowshoes/mixin/PowderSnowBlockMixin.java
@@ -1,9 +1,16 @@
 package ru.bastard.snowshoes.mixin;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.PowderSnowBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,11 +20,20 @@ import ru.bastard.snowshoes.items.ModItems;
 @Mixin(PowderSnowBlock.class)
 public class PowderSnowBlockMixin {
 
-    @Inject(method = "canEntityWalkOnPowderSnow", at = @At("HEAD"), cancellable = true)
-    private static void canEntityWalkOnPowderSnow(Entity p_154256_, CallbackInfoReturnable<Boolean> r) {
-        if (p_154256_ instanceof Player player) {
-            if (player.getInventory().getArmor(EquipmentSlot.FEET.getIndex()).is(ModItems.SNOWSHOES.get())) {
-                r.setReturnValue(true);
+    @Inject(method = "getCollisionShape", at = @At("HEAD"), cancellable = true)
+    public void getCollisionShape(BlockState p_154285_,
+                                  BlockGetter p_154286_,
+                                  BlockPos p_154287_,
+                                  CollisionContext p_154288_,
+                                  CallbackInfoReturnable<VoxelShape> cir) {
+        if (p_154288_ instanceof EntityCollisionContext ecc) {
+            Entity entity = ecc.getEntity();
+            if (entity != null) {
+                if (entity instanceof Player player) {
+                    if (player.getInventory().getArmor(EquipmentSlot.FEET.getIndex()).is(ModItems.SNOWSHOES.get())) {
+                        cir.setReturnValue(Shapes.block());
+                    }
+                }
             }
         }
     }
